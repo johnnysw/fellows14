1e026ef7950ccaa2cdeb14b10de35d905c94cd50
xiecheng <13136666328@163.com>
2017-10-19 15:34:58 +0800

    ES6

---
+++ ES6/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var a = 5;
+var add = function add(x, y) {
+  return x + y;
+};
+var b = 1,
+    c = 2,
+    d = 3;
+
+var e = 8;
+var f = 9;
---
+++ ES6/src/index.js
@@ -0,0 +1,5 @@
+let a = 5;
+let add = (x, y) => x + y;
+let [b, c, d] = [1, 2, 3];
+let e = 8;
+let f = 9;
