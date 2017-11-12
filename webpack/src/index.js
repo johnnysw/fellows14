2d54948b3f4403c38745b9818122f7bb6cb3f68c
wuying <wuy_123@126.com>
2017-11-12 13:49:48 +0800

    vue

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,4 @@
 /**
  * Created by apple on 17/11/12.
  */
-document.getElementById('div1').innerHTML = 'hello world111';
+document.getElementById('div1').innerHTML = 'hello world122211';
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry:{
         entry: "./src/index.js"
@@ -10,7 +11,15 @@ module.exports = {
     module:{
 
     },
-    plugins:[],
+    plugins:[
+        new HtmlWebpackPlugin({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template: "./src/index.html"
+        })
+    ],
     devServer:{
         contentBase: path.resolve(__dirname,'dist'),
         host:'127.0.0.1',
