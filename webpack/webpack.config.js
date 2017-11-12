4bdcb73ceb0a96269cdd087fb475a5e492d7ebe7
wuying <wuy_123@126.com>
2017-11-12 11:00:13 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,5 +6,15 @@ module.exports = {
     output:{
         path:path.resolve(__dirname,'dist'),
         filename: '[name].js'
+    },
+    module:{
+
+    },
+    plugins:[],
+    devServer:{
+        contentBase: path.resolve(__dirname,'dist'),
+        host:'127.0.0.1',
+        port:'8081',
+        compress:true
     }
 }
