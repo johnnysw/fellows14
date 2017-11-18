3dbaebbe88d98649f4a7257b2f2afc78c21f1c89
wuying <wuy_123@126.com>
2017-11-18 14:00:06 +0800

    vue

---
+++ vue-cli/vue-project/src/components/common/CommonHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+      <button>首页</button>
+      <h2>movie</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+      .header{
+        height    : 1rem;
+        background: rgb(33, 150, 243);
+        display   : flex;
+        color     : #fff;
+      }
+      .header button{
+        width : 1rem;
+        height: 1rem;
+        color : #fff;
+      }
+      .header h2{
+        flex       : 1;
+        text-align : center;
+        text-indent: -1rem;
+      }
+</style>
