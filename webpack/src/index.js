ef606485ad1f700b39c93e827d7e010ec1382317
wuying <wuy_123@126.com>
2017-12-02 15:44:06 +0800

    vue

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,2 +1,13 @@
-import css from './css/index.css'
+import css from './css/index.css';
+import scss from './css/test.scss';
 document.getElementById('div1').innerHTML = 'hello world122211';
+let aa = 20;
+const bb = 80;
+let fn = x => console.log(x);
+
+// import $ from "jquery";
+
+$("#aaa").html("faaaaaa");
+
+let user = require("../user.json");
+$("#aaa").html(`我的:${user.name}`);
