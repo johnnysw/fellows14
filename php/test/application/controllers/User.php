8c4d7e700f46c360762b719e1f5ed3cd7827f711
wuying <wuy_123@126.com>
2018-01-11 15:44:34 +0800

    vue

--- php/test/application/controllers/User.php
+++ php/test/application/controllers/User.php
@@ -3,28 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('User_model');
+//		$this->load->model('Class_model');
+	}
+
 	public function index()
 	{
 		$this->load->view('reg');
 	}
 
 	public function user_list(){
-		$this->load->model('User_model');
+
 		$users = $this->User_model->user_list();
 		$this->load->view('list',array('list'=>$users));
 	}
@@ -32,12 +24,36 @@ class User extends CI_Controller {
 	public function add(){
 		$name = $this->input->post('username');
 
-		$this->load->model('User_model');
 		$row = $this->User_model->add($name);
 		if($row > 0){
 			echo 'success';
 		}else{
 			echo 'fail';
+		}
+	}
+
+	public function del_user($id){
+//		$id = $this->input->get('id');
+
+		$rows = $this->User_model->del_user($id);
+		if($rows >0){
+			redirect('user/user_list');
+		}
+	}
+
+	public function update_user($id){
+		$result = $this->User_model->get_user_by_id($id);
+		$this->load->view('update_user',array('user'=>$result));
+	}
+
+	public function update($id){
+		$name = $this->input->post('username');
+		$rows = $this->User_model->update($id,$name);
+		if($rows > 0){
+			redirect('user/user_list');
+		}
+		else{
+			echo '修改失败';
 		}
 	}
 }
--- php/test/application/views/list.php
+++ php/test/application/views/list.php
@@ -11,12 +11,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <base href="<?php echo site_url(); ?>">
 </head>
 <body>
     <ul>
         <?php foreach ($list as $user){?>
 
-        <li><?php echo $user->id.",".$user->name?></li>
+        <li>
+            <?php echo $user->id.",".$user->name?>
+<!--            <a href="user/del_user?id=--><?php //echo $user->id?><!--">删除</a> |-->
+            <a href="user/del_user/<?php echo $user->id?>">删除</a>
+            <a href="user/update_user/<?php echo $user->id?>">修改</a>
+        </li>
 
         <?php }?>
     </ul>
