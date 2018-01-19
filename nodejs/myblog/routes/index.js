ef601a1b76e93658617c8d72d5a00b55e31e2fd4
wuying <wuy_123@126.com>
2018-01-19 15:47:45 +0800

    node

--- nodejs/myblog/routes/index.js
+++ nodejs/myblog/routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express111111',
+      title1:"hello"
+  });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+    var uname = req.body.uname;
+    var pwd = req.body.pwd;
+
+    if(uname == 'lisi' && pwd=='1234'){
+        res.render('index',{
+            name:uname
+        });
+    }
 });
 
 module.exports = router;
